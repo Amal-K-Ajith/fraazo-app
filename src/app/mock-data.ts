8de1b19b3d0887950d3aaadc44a103bf0ddc61b6
Amal-K-Ajith <amalajithk10@gmail.com>
2023-06-30 18:38:17 +0530

    css and img url changes & firbase

--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -1,38 +1,164 @@
-import { item } from "./items";
+import { item } from './items';
 
-export const VEGEs:item[]=[
-    {id:101, name:"onion", price:34, quantity:'1 Kg',imgUrl:'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/575/original/data?1635005372'},
-    {id:102, name:"Pumpkin", price:20, quantity:"1 Kg",imgUrl:'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/599/original/data?1635154399'},
-    {id:103, name:"Mushroom Button", price:24, quantity:"1kg",imgUrl:'https://fraazo-master.s3.ap-south-1.amazonaws.com/products/EMUS2.png'},
-    {id:104, name:"Beetroot", price:34, quantity:"200g",imgUrl:'https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VBEE12.png'},
-    {id:105, name:"Pumpkin Green", price:11, quantity:"250g",imgUrl:'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/101/original/data?1612451098'},
-    {id:106, name:"Fresh Salad Combo (250G Carrot + 500 G Cucumber)", price:45, quantity:"1Unit",imgUrl:'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/389/original/data?1624874955'},
-    {id:107, name:"Basil Italian", price:10, quantity:'50 g',imgUrl:'https://fraazo-master.s3.ap-south-1.amazonaws.com/products/EBAS11.png'},
-    {id:108, name:"Sweet Potato", price:15, quantity:"250g-300g",imgUrl:'https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VSWE12.png'},
-    {id:109, name:"Ladies Finger", price:13, quantity:"250g",imgUrl:'https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VLAD12.png'},
-]
+export const VEGEs: item[] = [
+  {
+    id: 101,
+    name: 'onion',
+    price: 34,
+    quantity: '1 Kg',
+    imgUrl:
+      '/assets/img/onion.jpg',
+  },
+  {
+    id: 102,
+    name: 'Pumpkin',
+    price: 20,
+    quantity: '1 Kg',
+    imgUrl:
+    '/assets/img/pumpkin.jpg',
+},
+  {
+    id: 103,
+    name: 'Mushroom Button',
+    price: 24,
+    quantity: '1kg',
+    imgUrl:
+    '/assets/img/mushroom_button.jpg',
+},
+  {
+    id: 104,
+    name: 'Beetroot',
+    price: 34,
+    quantity: '200g',
+    imgUrl:
+    '/assets/img/beetroot.jpg',
+},
+  {
+    id: 105,
+    name: 'Pumpkin Green',
+    price: 11,
+    quantity: '250g',
+    imgUrl:
+    '/assets/img/pumpkin_green.jpg',
+},
+//   {
+//     id: 106,
+//     name: 'Fresh Salad Combo (250G Carrot + 500 G Cucumber)',
+//     price: 45,
+//     quantity: '1Unit',
+//     imgUrl:
+//       'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/389/original/data?1624874955',
+//   },
+  {
+    id: 107,
+    name: 'Basil Italian',
+    price: 10,
+    quantity: '50 g',
+    imgUrl:
+    '/assets/img/basil_italian.jpg',
+},
+  {
+    id: 108,
+    name: 'Sweet Potato',
+    price: 15,
+    quantity: '250g-300g',
+    imgUrl:
+    '/assets/img/sweet_potato.jpg',
+},
+  {
+    id: 109,
+    name: 'Ladies Finger',
+    price: 13,
+    quantity: '250g',
+    imgUrl:
+    '/assets/img/ladies_finger.jpg',
+},
+];
 
-export const FRUITS:item[]=[
-    {id:201, name:"Smoothie Combo (Banana Robusta - 1Kg + Blueberry - 1 Pun...", price:34, quantity:'1 Kg',imgUrl:'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/442/original/data?1635063552'},
-    {id:202, name:"Longan", price:79, quantity:"250 g",imgUrl:'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/414/original/data?1628592194'},
-    {id:203, name:"Pineapple", price:49, quantity:"800g-1kg",imgUrl:'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/157/original/data?1595448197'},
-    {id:204, name:"Grape Fruit", price:59, quantity:"1Pc",imgUrl:'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/432/original/data?1629889583'},
-    {id:205, name:"Papaya", price:40, quantity:"1Pc",imgUrl:'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/092/original/data?1620114635'},
-    {id:206, name:"Papaya Green", price:21, quantity:"1Pc",imgUrl:'https://fraazo-master.s3.ap-south-1.amazonaws.com/products/VPAP12.png'},
-    {id:207, name:"Imported Cherry", price:299, quantity:'100 g',imgUrl:'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/421/original/data?1629108362'},
-    {id:208, name:"Avocado Indian", price:185, quantity:"1Pc",imgUrl:'https://fraazo-master.s3.ap-south-1.amazonaws.com/products/FAVO21.png'},
-    {id:209, name:"Apple Royal Gala", price:129, quantity:"2Pc",imgUrl:'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/006/original/data?1642616267'},
-]
+export const FRUITS: item[] = [
+//   {
+//     id: 201,
+//     name: 'Smoothie Combo (Banana Robusta - 1Kg + Blueberry - 1 Pun...',
+//     price: 34,
+//     quantity: '1 Kg',
+//     imgUrl:
+//       'https://s3.ap-south-1.amazonaws.com/fraazo-prod/products/product_images/000/000/442/original/data?1635063552',
+//   },
+  {
+    id: 202,
+    name: 'Longan',
+    price: 79,
+    quantity: '250 g',
+    imgUrl:
+    '/assets/img/longan.jpg',
+  },
+  {
+    id: 203,
+    name: 'Pineapple',
+    price: 49,
+    quantity: '800g-1kg',
+    imgUrl:
+    '/assets/img/pineapple.jpg',
+  },
+  {
+    id: 204,
+    name: 'Grape Fruit',
+    price: 59,
+    quantity: '1Pc',
+    imgUrl:
+    '/assets/img/grapefruit.jpg',
+  },
+  {
+    id: 205,
+    name: 'Papaya',
+    price: 40,
+    quantity: '1Pc',
+    imgUrl:
+    '/assets/img/papaya.jpg',
+  },
+  {
+    id: 206,
+    name: 'Papaya Green',
+    price: 21,
+    quantity: '1Pc',
+    imgUrl:
+    '/assets/img/papaya_green.jpg',
+  },
+  {
+    id: 207,
+    name: 'Imported Cherry',
+    price: 299,
+    quantity: '100 g',
+    imgUrl:
+    '/assets/img/cherry.jpg',
+  },
+  {
+    id: 208,
+    name: 'Avocado Indian',
+    price: 185,
+    quantity: '1Pc',
+    imgUrl:
+    '/assets/img/avocado.jpg',
+  },
+  {
+    id: 209,
+    name: 'Apple Royal Gala',
+    price: 129,
+    quantity: '2Pc',
+    imgUrl:
+    '/assets/img/apple_royal_gala.jpg',
+  },
+];
 
-export const OFFERS=[
-    {offerName:'',url:'',img:'assets/img/caro-1.png'},
-    {offerName:'',url:'',img:'assets/img/caro-2.png'},
-    {offerName:'',url:'',img:'assets/img/caro-3.png'},
-    {offerName:'',url:'',img:'assets/img/caro-4.png'},
-    {offerName:'',url:'',img:'assets/img/caro-5.png'},
-    {offerName:'',url:'',img:'assets/img/caro-6.png'},
-    {offerName:'',url:'',img:'assets/img/caro-7.png'},
-    {offerName:'',url:'',img:'assets/img/caro-8.png'},
-    {offerName:'',url:'',img:'assets/img/caro-9.png'},
-    {offerName:'',url:'',img:'assets/img/caro-10.png'}
-]
+export const OFFERS = [
+  { offerName: '', url: '', img: 'assets/img/caro-1.png' },
+  { offerName: '', url: '', img: 'assets/img/caro-2.png' },
+  { offerName: '', url: '', img: 'assets/img/caro-3.png' },
+  { offerName: '', url: '', img: 'assets/img/caro-4.png' },
+  { offerName: '', url: '', img: 'assets/img/caro-5.png' },
+  { offerName: '', url: '', img: 'assets/img/caro-6.png' },
+  { offerName: '', url: '', img: 'assets/img/caro-7.png' },
+  { offerName: '', url: '', img: 'assets/img/caro-8.png' },
+  { offerName: '', url: '', img: 'assets/img/caro-9.png' },
+  { offerName: '', url: '', img: 'assets/img/caro-10.png' },
+];
